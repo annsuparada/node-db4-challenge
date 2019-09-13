db8828f4e8a583e5f42267ea92b61d692661f903
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-12 19:56:17 -0500

    MVP

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -14,13 +14,13 @@ function getShoppingList(recipe_id) {
     return db('recipes as R')
         .join('recipe_ingrediant as RI', 'R.id', 'RI.recipe_id')
         .join('ingrediants as I', 'I.id', 'RI.ingrediant_id')
-        .select('R.recipe_name', 'RI.quantity', 'I.ingrediant_name' )
+        .select('RI.quantity', 'I.ingrediant_name' )
         .where( 'RI.recipe_id', recipe_id)
 }
 
 function getInstructions(recipe_id) {
     return db('recipes as R')
         .join('instructions as I', 'R.id', 'I.recipe_id')
-        .select('I.steps', 'R.recipe_name')
+        .select('I.steps')
         .where('I.recipe_id', recipe_id)
 }
